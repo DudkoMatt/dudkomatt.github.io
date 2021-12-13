05dca6dea0863937a46146865292b3d5aa45eeb9
DudkoMatt <github.dudko.matt@mail.ru>
2021-12-13 22:10:28 +0300

    chore: Added js comments

--- JS/letter_template_generator/extensions/notie.js
+++ JS/letter_template_generator/extensions/notie.js
@@ -1,14 +1,31 @@
+// Use-case сценраний:
+//
+// Для работы со списком идей, который хотелось
+// обсудить со мной по почте, стоит добавить систему
+// уведомлений, чтобы пользователю было удобнее
+// понимать, какие события происходят на странице.
+//
+// Например:
+// 1. Собитие (не)успешного добавления элемента в список;
+// 2. Событие загрузки сохраненных элементов из хранилища;
+// 3. Подтверждение удаления всех элементов из списка;
+// 4. Событие начала/окончания рендеринга результата;
+// 5. Подсказка при попытке сохранения пустой формы.
+//
+// Для решения данной задачи подходит UI библиотека для JS "notie":
+// https://github.com/jaredreich/notie
+
 const DURATION_MESSAGES = 1.5;
 const POSITION = 'top';
 
 export function onAdd() {
-    notie.alert({
-        type: 'success',
-        text: 'Successfully added',
-        stay: false,
-        time: DURATION_MESSAGES,
-        position: POSITION
-    });
+    notie.alert({                          // notie.alert - используется для отображения простого уведомления
+        type: 'success',                   // type        - задает цвет уведомлению
+        text: 'Successfully added',        // text        - текст уведомления
+        stay: false,                       // stay        - следует ли остаться уведомлению навсегда или нет
+        time: DURATION_MESSAGES,           // time        - время, через которое уведомление исчезнет
+        position: POSITION                 // position    - положение баннера (снизу или сверху)
+    });                                    //
 }
 
 export function onError() {
@@ -32,20 +49,22 @@ export function onEmptyForm() {
 }
 
 export function onErase(submitCallback) {
-    notie.confirm({
-        text: "You are going to erase everything. Are you sure?",
-        submitText: "Yes",
-        cancelText: "Cancel",
-        position: POSITION
-    }, submitCallback, () => {
-        notie.alert({
-            type: 'warning',
-            text: 'Canceled',
-            stay: false,
-            time: DURATION_MESSAGES,
-            position: POSITION
-        });
-    });
+    notie.confirm({                                                // notie.confirm - подтверждение выполняемой операции
+        text: "You are going to erase everything. Are you sure?",  // text          - текст уведомления
+        submitText: "Yes",                                         // submitText    - текст кнопки подтверждения
+        cancelText: "Cancel",                                      // cancelText    - текст кнопки отмены
+        position: POSITION                                         // position      - положение баннера (снизу/сверху)
+    },                                                             //
+    submitCallback,                                                // submitCallback - callback при подтверждении
+    () => {                                                        // cancelCallbackOptional - при отмене
+        notie.alert({                                              //
+            type: 'warning',                                       //
+            text: 'Canceled',                                      //
+            stay: false,                                           //
+            time: DURATION_MESSAGES,                               //
+            position: POSITION                                     //
+        });                                                        //
+    });                                                            //
 }
 
 export function onEraseDone() {
